3646bd7e581e3fc11eba7315d51fa484adc81f30
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2015-06-28 08:43:28 +0800

    Initial project structure.

---
+++ app/src/androidTest/java/com/jitelec1b1516/layoutbasics/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.jitelec1b1516.layoutbasics;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
